913b79fbfe2dd75b1d816530ca07cc5e6a345271
Ilavarasu Pandian <ilavarasu.pandian1@t-mobile.com>
2020-01-24 22:54:06 -0800

    git 13

--- main.js
+++ main.js
@@ -1051,8 +1051,8 @@ var NtmUnavDropdownComponent = /** @class */ (function (_super) {
         }*/
         if (e.type === 'touchend' && !this.mobileView) {
             if (!this.menuOpened) {
+                e.preventDefault();
                 setTimeout(function () {
-                    e.preventDefault();
                     _this.menuOpened = true;
                     _this.changeRef.detectChanges();
                     return;
@@ -1512,7 +1512,7 @@ var IconLink;
 /*!***********************************************************************************************************!*\
   !*** D:/TMO_PROJECTS/TMNG/tmo-aem-ui-integration/tmo-ng/libs/shared/digital-unav/src/lib/models/index.ts ***!
   \***********************************************************************************************************/
-/*! exports provided: Header, HeaderUserLinks, IconLink, Link, LinkUrl, UnavComponent */
+/*! exports provided: LinkUrl, Header, HeaderUserLinks, IconLink, Link, UnavComponent */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
