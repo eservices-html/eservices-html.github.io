7d99f1b47354abc02ac8d64973a8f046e0d8d9c0
Ilavarasu Pandian <ilavarasu.pandian1@t-mobile.com>
2020-01-24 19:42:46 -0800

    test code 1

--- main.js
+++ main.js
@@ -1038,6 +1038,7 @@ var NtmUnavDropdownComponent = /** @class */ (function (_super) {
         var parentSrcId = parentEle ? parentEle['id'] : '';
         var isnavLink = srcId.startsWith('digital-header-nav-link-head') || parentSrcId.startsWith('digital-header-nav-link-head');
         if (!this.mobileView && isnavLink && items > 0) {
+            this.closeMenu.emit();
             if (!this.menuOpened) {
                 e.preventDefault();
                 this.closeMenu.emit();
@@ -1493,7 +1494,7 @@ var IconLink;
 /*!***********************************************************************************************************!*\
   !*** D:/TMO_PROJECTS/TMNG/tmo-aem-ui-integration/tmo-ng/libs/shared/digital-unav/src/lib/models/index.ts ***!
   \***********************************************************************************************************/
-/*! exports provided: LinkUrl, Header, HeaderUserLinks, IconLink, Link, UnavComponent */
+/*! exports provided: Header, HeaderUserLinks, IconLink, Link, LinkUrl, UnavComponent */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
